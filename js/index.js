a42e96a2f81063cf568c1f9bfdcb0562d78f44d1
Nitin <nitinshankarmadhu@gmail.com>
2022-12-04 19:57:56 -0500

    Added features

--- js/index.js
+++ js/index.js
@@ -34,6 +34,27 @@ for (let i = 0; i < 2; i++)
 {
     outputs[i].textContent = forms[i].value
 }
+
+let contactlink = document.querySelectorAll(".icontact-link")
+
+if (forms[2] != "")
+contactlink[0].href = "mailto:"+ forms[2].value
+
+if (forms[3]!= "")
+contactlink[1].href ="https://github.com/"+ forms[3].value;
+
+if (forms[4]!="")
+contactlink[2].href ="https://instagram.com/"+ forms[4].value;
+else 
+$(contactlink[2]).remove();
+
+if (forms[5].value !=""){
+document.querySelector(".about-p").textContent =forms[5].value 
+document.querySelector("#about h2").textContent ="About "+ forms[0].value
+}
+else
+$("#about").remove();   
+
 if (workcount!=document.getElementsByClassName("card").length){
     for (let i = 0; i<workcount; i++)
     {
@@ -48,9 +69,9 @@ outputs = document.querySelectorAll(".output")
 form_links = document.querySelectorAll(".form-link") //refreshing list of divs and forms
 links = document.querySelectorAll(".link")
 
-for (let i = 2; i < forms.length; i++)
+for (let i = 6; i < forms.length; i++)
 {
-    outputs[i].textContent = forms[i].value
+    outputs[i-4].textContent = forms[i].value
 }
 
 
@@ -75,15 +96,11 @@ $("#edit").click(function(){
 })
 
 
-$("#code").click(function(){
-    let code = '<!DOCTYPE html>\n<html lang="en"><head>\n<meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Portfolio Page</title>'
-    code+='<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"></link>'
-    code += "<style>\n"
+$("#code").click(function(){ //scraping my own site
+    let code = '<!DOCTYPE html>\n<html lang="en"><head>\n<meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Portfolio Page</title>'+'<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"></link>'+ "<style>\n"
     for (let i=0; i<document.styleSheets[0].cssRules.length; i++)
     code += document.styleSheets[0].cssRules[i].cssText+"\n"
-    code+= "</style>\n</head>\n<body>\n"
-    code += $(".page").html()
-    code += '</body>\n</html>'
+    code+= "</style>\n</head>\n<body>\n" + $(".page").html() + '</body>\n</html>'
     $(".page").toggle()
     $("#edit").toggle()
     $("#code").toggle()
